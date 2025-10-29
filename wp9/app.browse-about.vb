3c586a76085eecd376df37e85fbf2aafe56655c0
micraia <59067041+iCloExecutable@users.noreply.github.com>
2025-10-29 03:08:01 +0300

    clean

--- wp9/app.browse-about.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class browserabout
-	Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-
-	End Sub
-End Class
--- wp9/ui.settings-devsettings.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class devmenu
-    Private Sub devmenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        recent.Text = My.Settings.recntver
-        current.Text = My.Resources.version
-    End Sub
-End Class
